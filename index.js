a29cc506eb1ed92062472f7e04f401933f273248
AnaSad-front <anastasiasadyreva1@gmail.com>
2026-02-03 17:59:55 +0300

    final

--- index.js
+++ index.js
@@ -4,41 +4,6 @@ const url = require('url');
 const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
-//////////////////////////////////////
-// FILES
-/*
-// blocking, synchronous way
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-console.log(textIn);
-const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("File written successfully");
-
-// non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("Error reading start file:", err);
-
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    if (err) return console.log("Error reading second file:", err);
-
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      if (err) return console.log("Error reading append file:", err);
-
-      const finalText = `${data2}\n${data3}`;
-      fs.writeFile("./txt/finalOutput.txt", finalText, (err) => {
-        if (err) return console.log("Error writing final output file:", err);
-        console.log("Final output file written successfully");
-      });
-    });
-  });
-});
-
-console.log("Reading files...");
-*/
-
-//////////////////////////////////////
-// SERVER
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   'utf-8',
