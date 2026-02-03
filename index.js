05b3eddbc7d449321f20872f6fea50e41b44c529
AnaSad-front <anastasiasadyreva1@gmail.com>
2026-02-03 14:05:15 +0300

    creating a simple web server

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const fs = require("fs");
+const http = require("http");
 
+//////////////////////////////////////
+// FILES
+/*
 // blocking, synchronous way
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-// const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync("./txt/output.txt", textOut);
-// console.log("File written successfully");
+const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+console.log(textIn);
+const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync("./txt/output.txt", textOut);
+console.log("File written successfully");
 
 // non-blocking, asynchronous way
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
@@ -27,3 +31,15 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 });
 
 console.log("Reading files...");
+*/
+
+//////////////////////////////////////
+// SERVER
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listening to request on port 8000");
+});
